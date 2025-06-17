43cf422dc9ee1c6fa7bb2ee08f4d147e65503d84
Panos Georgantas <panos@georgantas.info>
2025-06-17 19:21:41 +0300

    add check if efs already mounted

--- beanstalk/.platform/hooks/prebuild/20_efs.sh
+++ beanstalk/.platform/hooks/prebuild/20_efs.sh
@@ -3,7 +3,12 @@
 EFS_MOUNT_=$(/opt/elasticbeanstalk/bin/get-config environment -k EFS_MOUNT 2>/dev/null)
 EFS_DNS=$(/opt/elasticbeanstalk/bin/get-config environment -k EFS_DNS 2>/dev/null)
 
-if [[ -n "$EFS_MOUNT" ]]; then
+if [[ -n "${EFS_MOUNT}" ]]; then
       mkdir -p ${EFS_MOUNT}
-      mount -t nfs4 -o nfsvers=4.1 ${EFS_DNS}:/ ${EFS_MOUNT}
+      if grep -qs '${EFS_DNS} ' /proc/mounts; then
+          echo "${EFS_DNS} is already mounted."
+      else
+          echo "Mounting ${EFS_DNS}"
+          mount -t nfs4 -o nfsvers=4.1 ${EFS_DNS}:/ ${EFS_MOUNT}
+      fi
 fi
